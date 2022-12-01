0255cf12d68a9445033d0dcd29ce77798e8abd95
Ernest K <ernio789@gmail.com>
2022-12-01 20:54:49 +0100

    chart class rebuild

---
+++ src/typescript/Axes.ts
@@ -0,0 +1,79 @@
+import * as d3 from "d3";
+import { ChartDimensions } from "./interfaces";
+
+export default class Axes {
+  private _yScale;
+  private _xScale;
+  private _yAxis;
+  private _xAxis;
+
+  private _yAccessor = (data) => data.length;
+
+  constructor(
+    private _buckets: number[],
+    private _chartDimensions: ChartDimensions,
+    private MAX: number
+  ) {
+    this.update();
+  }
+
+  private _createYScale(): void {
+    this._yScale = d3
+      .scaleLinear()
+      .domain([0, d3.max(this._buckets as number[], this._yAccessor)])
+      .range([
+        this._chartDimensions.height - 2 * this._chartDimensions.margins,
+        0,
+      ])
+      .nice();
+  }
+
+  private _createXScale(): void {
+    this._xScale = d3
+      .scaleLinear()
+      .domain([0, this.MAX])
+      .range([
+        0,
+        this._chartDimensions.width - 2 * this._chartDimensions.margins,
+      ]);
+  }
+
+  private _createYAxis(): void {
+    this._yAxis = d3.axisLeft(this._yScale);
+  }
+
+  private _createXAxis(): void {
+    this._xAxis = d3.axisBottom(this._xScale).ticks(Math.floor(this.MAX / 2));
+  }
+
+  public get yScale() {
+    return this._yScale;
+  }
+
+  public get xScale() {
+    return this._xScale;
+  }
+
+  public get yAxis() {
+    return this._yAxis;
+  }
+
+  public get xAxis() {
+    return this._xAxis;
+  }
+
+  public set buckets(buckets) {
+    this._buckets = buckets;
+  }
+
+  public set chartDimensions(chartDimensions) {
+    this._chartDimensions = chartDimensions;
+  }
+
+  public update() {
+    this._createYScale();
+    this._createXScale();
+    this._createYAxis();
+    this._createXAxis();
+  }
+}
---
+++ src/typescript/Chart.ts
@@ -0,0 +1,129 @@
+import { chartDimensions } from "./chartDimensions";
+import Axes from "./Axes";
+import * as d3 from "d3";
+
+export default class Chart {
+  private MAX = 64;
+
+  private _chartDimensions = chartDimensions;
+  private _buckets;
+  private _axes;
+  private _svg;
+  private _barGroup;
+  private _xAxisGroup;
+  private _yAxisGroup;
+
+  private _yAccessor = (data) => data.length;
+
+  constructor(
+    private _chartContainer: HTMLElement,
+    private _meanSamples: number[]
+  ) {}
+
+  public setMeanSamples(meanSamples: number[]) {
+    this._meanSamples = meanSamples;
+  }
+
+  private _createBuckets() {
+    const bin = d3
+      .bin()
+      .domain([0, this.MAX])
+      .thresholds(Math.floor(this.MAX / 2));
+
+    this._buckets = bin(this._meanSamples);
+  }
+
+  private _createSvg() {
+    this._svg = d3
+      .select(this._chartContainer)
+      .append("svg")
+      .attr("width", this._chartDimensions.width)
+      .attr("height", this._chartDimensions.height);
+  }
+
+  private _createBarGroup() {
+    this._barGroup = this._svg
+      .append("g")
+      .classed("bar-group", true)
+      .attr(
+        "transform",
+        `translate(${this._chartDimensions.margins}, ${this._chartDimensions.margins})`
+      )
+      .append("g");
+  }
+
+  private _createBar() {
+    this._barGroup
+      .selectAll("rect")
+      .data(this._buckets)
+      .join("rect")
+      .transition()
+      .attr("x", (d) => this._axes.xScale(d.x0) + 1)
+      .attr("width", (d) =>
+        d3.max([0, this._axes.xScale(d.x1) - this._axes.xScale(d.x0) - 2])
+      )
+      .attr("y", (d) => this._axes.yScale(this._yAccessor(d)))
+      .attr(
+        "height",
+        (d) =>
+          this._chartDimensions.height -
+          2 * this._chartDimensions.margins -
+          this._axes.yScale(this._yAccessor(d))
+      );
+  }
+
+  private _createYAxisGroup() {
+    this._yAxisGroup = this._svg
+      .append("g")
+      .attr(
+        "transform",
+        `translate(${this._chartDimensions.margins}, ${this._chartDimensions.margins})`
+      );
+  }
+
+  private _createXAxisGroup() {
+    this._xAxisGroup = this._svg
+      .append("g")
+      .attr(
+        "transform",
+        `translate(${this._chartDimensions.margins}, ${
+          this._chartDimensions.height - this._chartDimensions.margins
+        })`
+      );
+  }
+
+  private _appendYAxis(yAxis) {
+    this._yAxisGroup.transition().call(yAxis);
+  }
+
+  private _appendXAxis(xAxis) {
+    this._xAxisGroup.transition().call(xAxis);
+  }
+
+  public create() {
+    this._createBuckets();
+    this._createSvg();
+    this._createYAxisGroup();
+    this._createXAxisGroup();
+
+    this._axes = new Axes(this._buckets, this._chartDimensions, this.MAX);
+
+    this._appendYAxis(this._axes.yAxis);
+    this._appendXAxis(this._axes.xAxis);
+
+    this._createBarGroup();
+    this._createBar();
+  }
+
+  public update() {
+    this._createBuckets();
+
+    this._axes.buckets = this._buckets;
+    this._axes.update();
+
+    this._appendYAxis(this._axes.yAxis);
+    this._appendXAxis(this._axes.xAxis);
+
+    this._createBar();
+  }
+}
